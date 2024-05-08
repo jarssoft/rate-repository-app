ac9eb8c3e63ac1c44ab3359c33c0a65f8f2f869b
Jari Saari <jari.m.saari@gmail.com>
2024-05-08 15:50:25 +0300

    10.12

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,9 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import Constants from "expo-constants";
 
 const createApolloClient = () => {
+  console.log(Constants.expoConfig.extra.apolloUri);
   return new ApolloClient({
     //uri: "http://192.168.1.100:4000/graphql",
-    uri: "http://192.168.1.109:4000/graphql",
+    //uri: "http://192.168.1.109:4000/graphql",
+    uri: Constants.expoConfig.extra.apolloUri,
     cache: new InMemoryCache(),
   });
 };
