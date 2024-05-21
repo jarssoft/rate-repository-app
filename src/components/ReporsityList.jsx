8441cf6a89739d4d76eafee022e24aea38a30907
Jari Saari <jari.m.saari@gmail.com>
2024-05-21 18:33:51 +0300

    sorting in states

--- src/components/ReporsityList.jsx
+++ src/components/ReporsityList.jsx
@@ -1,8 +1,11 @@
 import useRepositories from "../hooks/useRepositories";
 import RepositoryListContainer from "./ReporsityListContainer";
+import { useState } from "react";
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories("RATING_AVERAGE", "DESC");
+  const [by, setBy] = useState("RATING_AVERAGE");
+  const [direction, setDirection] = useState("DESC");
+  const { repositories } = useRepositories(by, direction);
 
   return <RepositoryListContainer repositories={repositories} />;
 };
